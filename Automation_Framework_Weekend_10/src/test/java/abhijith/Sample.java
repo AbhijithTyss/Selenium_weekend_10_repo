04d64105dee200f5237bfa9116ca0373122caa59
AbhijithTyss <abhijith.styss@gmail.com>
2025-06-07 12:09:03 +0530

    Added sample class

---
+++ Automation_Framework_Weekend_10/src/test/java/abhijith/Sample.java
@@ -0,0 +1,12 @@
+package abhijith;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Sample {
+	@Test
+	public void launch() {
+		WebDriver driver=new ChromeDriver();
+	}
+}
